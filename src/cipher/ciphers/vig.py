fd5b9a25d9bea2c2133eb1bf97082bf98601c1ab
marvhus <44480334+marvhus@users.noreply.github.com>
2022-08-10 21:58:20 +0200

    if this does not work, I'm going to cry

--- src/cipher/ciphers/vig.py
+++ src/cipher/ciphers/vig.py
@@ -14,51 +14,48 @@ class Vig(Cipher): #make sure you change this from text to your cipher
     name = 'Vignere Cipher' #change the name
     type = 'cipher'
 
-    # https://gist.github.com/dssstr/aedbb5e9f2185f366c6d6b50fad3e4a4?permalink_comment_id=4035102#gistcomment-4035102
-    def vigenere(
-        text: str, 
-        key: str, 
-        alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-        encrypt=True
-    ):
-        result = ''
-        for i in range(len(text)):
-            letter_n = alphabet.index(text[i])
-            key_n = alphabet.index(key[i % len(key)])
-            if encrypt:
-                value = (letter_n + key_n) % len(alphabet)
-            else:
-                value = (letter_n - key_n) % len(alphabet)
-            result += alphabet[value]
-        return result
-
+    alphabet = [chr(i).upper() for i in range(ord('a'), ord('z')+1)]
 
     def encode(args):
-        text = args.text
-        key = args.key
+        text = args.text.upper()
+        key = args.key.upper()
 
         if not text:
             return {'text': "No input text", 'success': False}
 
         if not key:
             return {'text': "No key", 'success': False}
-        
-        output = Vig.vigenere(text=text, key=key, encrypt=True)
+       
+        output = []
+        for t,k in zip(text, cycle(key)):
+            if t not in Vig.alphabet:
+                output.append(t)
+                continue
+            x = ( ord(t) + ord(k) ) % 26
+            x += ord('A')
+            output.append( chr(x).lower() )
 
-        return {'text': output, 'success': True}
+        return {'text': "".join(output), 'success': True}
 
     def decode(args):
-        text = args.text
-        key = args.key
+        text = args.text.upper()
+        key = args.key.upper()
 
         if not text:
             return {'text': "No input text", 'success': False}
         if not key:
             return {'text': "No key", 'success': False}
 
-        output = Vig.vigenere(text=text, key=key, encrypt=False)
+        output = []
+        for t,k in zip(text, cycle(key)):
+            if t not in Vig.alphabet:
+                output.append(t)
+                continue
+            x = ( ord(t) - ord(k) + 26 ) % 26
+            x += ord('A')
+            output.append( chr(x).lower() )
 
-        return {'text': output, 'success': True}
+        return {'text': "".join(output), 'success': True}
 
     def print_options():
         #Edit this section as needed for your specific encoding / decoding.
