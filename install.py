e6f295551c3a9f79e492857360db9c5a774d2eac
marvhus <marvahus@proton.me>
2022-10-13 13:24:50 +0200

    optimized code

--- install.py
+++ install.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def main():
-
 # Imports.
-    import sys # System stuff.
+import sys # System stuff.
 import os # Operating System functions.
 
-menu=True
-while menu: # Prints the menu.
+class Distros:
+    @staticmethod
+    def debian():
+        return [
+            'sudo apt update',
+            'sudo apt-get install -y python3 python3-pip python-dev',
+        ]
+    @staticmethod
+    def arch():
+        return [
+            'sudo packman -Syu',
+            'sudo packman -S python python-pip',
+        ]
+    @staticmethod
+    def void():
+        return Distros.debian()
+
+def handle_distros():
     print("""
         - Debian
             (if below type 'Debian')
@@ -17,62 +31,68 @@ while menu: # Prints the menu.
             - Kali
             - Mint
         - Arch
-        - Garuda
+            (if bellow type 'Arch'
+            - Garuda
+            - Manjaro 
         - Void
         
         **Case sensitive**
         """)
-    ans=input(">_ What is your operating system?: ")
+    ans=input(">_ What is your operating system?: ").lower()
+
+    if 'debian' in ans:
+        return Distros.debian()
+    elif 'arch' in ans:
+        return Distros.arch()
+    elif 'void' in ans:
+        return Distros.void()
     
-    # Migrating from os.system once foundation has been built.
-    if ans=="Debian": # Installs for Debian.
-        os.system("sudo apt update")
-        os.system("sudo apt-get install -y python3 python3-pip python-dev")
-        os.system("pip install -r requirements.txt")
-        os.system('rm -rf ~/$USERNAME/.Cryptex')
-        os.system('mkdir ~/$USERNAME/.Cryptex')
-        os.system('cp -r . ~/$USERNAME/.Cryptex')
-        os.system('echo "alias cryptex="python3 ~/$USERNAME/.Cryptex/src/main.py"" >> ~/$USERNAME/.zshrc')
-        print("Installation finished.\nRestart the terminal and type 'cryptex' to run the program")
-        exit()
-    elif ans=="Arch": # Installs for Arch.
-        os.system("sudo pacman -Syu")
-        os.system("sudo pacman -S python python-pip")
-        os.system("python3 -m pip install -r requirements.txt")
-        os.system('rm -rf ~/$USERNAME/.Cryptex')
-        os.system('mkdir ~/$USERNAME/.Cryptex')
-        os.system('cp -r . ~/$USERNAME/.Cryptex')
-        os.system('echo "alias cryptex="python3 ~/$USERNAME/.Cryptex/src/main.py"" >> ~/$USERNAME/.zshrc')
-        print("Installation finished.\nRestart the terminal and type 'cryptex' to run the program")
-        exit()
-    elif ans=="Garuda": # Installs for Garuda.
-        os.system("sudo pacman -Syu")
-        os.system("sudo pacman -S python python-pip")
-        os.system("python3 -m pip install -r requirements.txt")
-        os.system('rm -rf ~/$USERNAME/.Cryptex')
-        os.system('mkdir ~/$USERNAME/.Cryptex')
-        os.system('cp -r . ~/$USERNAME/.Cryptex')
-        os.system('echo "alias cryptex="python3 ~/$USERNAME/.Cryptex/src/main.py"" >> ~/$USERNAME/.zshrc')
-        print("Installation finished.\nRestart the terminal and type 'cryptex' to run the program")
-        exit()
-    elif ans=="Void": # Installs for Void.
-        os.system("sudo apt update")
-        os.system("sudo apt-get install -y python3 python3-pip python-dev")
-        os.system("sudo apt update")
-        os.system("sudo apt-get install -y python3 python3-pip python-dev")
-        os.system("pip install -r requirements.txt")
-        os.system('rm -rf ~/$USERNAME/.Cryptex')
-        os.system('mkdir ~/$USERNAME/.Cryptex')
-        os.system('cp -r . ~/$USERNAME/.Cryptex')
-        os.system('echo "alias cryptex="python3 ~/$USERNAME/.Cryptex/src/main.py"" >> ~/$USERNAME/.zshrc')
-        print("Installation finished.\nRestart the terminal and type 'cryptex' to run the program")
-        exit()
-    ans = None
-else:
-   print("\n Not Valid Choice Try again...")
+    print(f'Unsuported distro: {ans}')
+    exit(1)
+
+def check_shell_config(location):
+    with open(location, 'rt') as f:
+        check = 'alias cryptex' in f.read()
+    return check
+    
+def handle_shell():
+    shell = os.environ['SHELL']
+    command = 'echo "alias cryptex="python3 ~/.Cryptex/src/main.py"' 
+    path = ''
+    user = os.environ['HOME']
+    if 'bash' in shell:
+        path = f'{user}/.bashrc'
+    elif 'zsh' in shell:
+        path = f'{user}/.zshrc'
+    else:
+        print(f'Unsuported shell: {shell}')
+        exit(1)
+
+    if check_shell_config(path):
+        print(f'\nAlias already exists in {path}\n')
+        return ''
+
+    return f'{command} >> {path}'
+    
+def main():
+    commands = []
+
+    commands += handle_distros()
+    commands += [
+        'pip install -r requirements.txt',
+        'rm -rf ~/.Cryptex',
+        'mkdir ~/.Cryptex',
+        'cp -r . ~/.Cryptex',
+    ]
+    commands += [handle_shell()]
+
+    for c in commands:
+        os.system(c)
+    
+    print("Installation finished.\nRestart the terminal and type 'cryptex' to run the program")
+    exit()
+
 if __name__ == '__main__':
-    import sys # System stuff.
-    import os # Operating System functions.
     try:
         main()
     except KeyboardInterrupt:
